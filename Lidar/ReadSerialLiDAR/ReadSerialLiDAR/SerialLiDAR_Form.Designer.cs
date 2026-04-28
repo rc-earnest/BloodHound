173430372545c6c4f6febf2b28214509c73b99cc
JonCra <jonas.d.cram@gmail.com>
2026-04-28 10:32:16 -0600

    Init C# WinForm for reading/parsing data from LiDAR

---
+++ Lidar/ReadSerialLiDAR/ReadSerialLiDAR/SerialLiDAR_Form.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ReadSerialLiDAR
+{
+    partial class SerialLiDAR_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PortsComboBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.CommPortStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PortsComboBox
+            // 
+            this.PortsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortsComboBox.FormattingEnabled = true;
+            this.PortsComboBox.Location = new System.Drawing.Point(67, 335);
+            this.PortsComboBox.Name = "PortsComboBox";
+            this.PortsComboBox.Size = new System.Drawing.Size(143, 28);
+            this.PortsComboBox.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CommPortStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 418);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 32);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // CommPortStatusLabel
+            // 
+            this.CommPortStatusLabel.Name = "CommPortStatusLabel";
+            this.CommPortStatusLabel.Size = new System.Drawing.Size(187, 25);
+            this.CommPortStatusLabel.Text = "CommPortStatusLabel";
+            // 
+            // SerialLiDAR_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.PortsComboBox);
+            this.Name = "SerialLiDAR_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LiDAR Serial Reader";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortsComboBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel CommPortStatusLabel;
+    }
+}
+
---
+++ Lidar/ReadSerialLiDAR/ReadSerialLiDAR/SerialLiDAR_Form.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace ReadSerialLiDAR
+{
+    public partial class SerialLiDAR_Form : Form
+    {
+        // ----------
+        // Variables
+        // ----------
+        int selectedIndex;
+
+        // ----------
+        // Startup
+        // ----------
+        public SerialLiDAR_Form()
+        {
+            InitializeComponent();
+            SetDefaults();
+            GetPorts();
+        }
+        void SetDefaults()
+        {
+
+        }
+        void GetPorts()
+        {
+
+            SetPortName();
+        }
+        void SetPortName()
+        {
+            try
+            {
+                // Translate selected index on combo box to selected port
+                selectedIndex = PortsComboBox.SelectedIndex;
+                if (serialPort1.IsOpen)
+                {
+                    // Display selected port
+                    CommPortStatusLabel.Text = $"Active Port: {serialPort1.PortName}";
+                }
+                else
+                {
+                    // Display that NO port has been selected
+                    CommPortStatusLabel.Text = "Active Port: None";
+                }
+            }
+            catch
+            {
+                // Show error message for debugging
+                Console.WriteLine("No ports available");
+            }
+        }
+    }
+}
