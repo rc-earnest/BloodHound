e184516006a7d2085b242dc0dfd08d77e1d721b5
JonCra <jonas.d.cram@gmail.com>
2026-04-30 11:05:22 -0600

    Added project header

--- Lidar/ReadSerialLiDAR/ReadSerialLiDAR/SerialLiDAR_Form.cs
+++ Lidar/ReadSerialLiDAR/ReadSerialLiDAR/SerialLiDAR_Form.cs
@@ -12,7 +12,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-
+/* PROJECT BLOODHOUND - LiDAR SERIAL RECEPTION TESTING & DOCUMENTATION
+ * Jonas Cram
+ * 5th Semester - Spring 2026
+ * Robotics and Communication Systems Engineering Technology (RCET)
+ * Idaho State University - College of Technology
+ */
 namespace ReadSerialLiDAR
 {
     public partial class SerialLiDAR_Form : Form
